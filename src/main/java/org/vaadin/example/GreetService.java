cb3ad6ae78d572f3c085940161505c5f3aafa1e7
DGP <dgarcia@ctgrupo.com>
2022-12-24 16:23:32 +0100

    F6

--- src/main/java/org/vaadin/example/GreetService.java
+++ src/main/java/org/vaadin/example/GreetService.java
@@ -15,4 +15,12 @@ public class GreetService implements Serializable {
             return "Hello " + name;
         }
     }
+
+    public String F6(String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello anonymous user";
+        } else {
+            return "Hello " + name;
+        }
+    }
 }
