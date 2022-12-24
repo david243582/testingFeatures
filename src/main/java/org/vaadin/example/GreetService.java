0ac5e27efed74864b9109bee4bd86581bd2870c4
DGP <dgarcia@ctgrupo.com>
2022-12-24 16:07:56 +0100

    dev into master

--- src/main/java/org/vaadin/example/GreetService.java
+++ src/main/java/org/vaadin/example/GreetService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetService implements Serializable {
 
+    //Merge dev into master
     public String greet(String name) {
         if (name == null || name.isEmpty()) {
             return "Hello anonymous user";
