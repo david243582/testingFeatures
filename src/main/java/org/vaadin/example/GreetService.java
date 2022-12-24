7edd1985b556961ddfb7561cf4b8a895e85ca311
DGP <dgarcia@ctgrupo.com>
2022-12-24 16:45:50 +0100

    F9P1

--- src/main/java/org/vaadin/example/GreetService.java
+++ src/main/java/org/vaadin/example/GreetService.java
@@ -23,4 +23,12 @@ public class GreetService implements Serializable {
             return "Hello " + name;
         }
     }
+
+    public String F9P1(String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello anonymous user";
+        } else {
+            return "Hello " + name;
+        }
+    }
 }
