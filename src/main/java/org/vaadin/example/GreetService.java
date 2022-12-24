375478383ef68e21be92ac790e6cc25abfe6faa4
DGP <dgarcia@ctgrupo.com>
2022-12-24 15:40:44 +0100

    feature2

--- src/main/java/org/vaadin/example/GreetService.java
+++ src/main/java/org/vaadin/example/GreetService.java
@@ -14,4 +14,12 @@ public class GreetService implements Serializable {
             return "Hello " + name;
         }
     }
+
+    public String feature2(String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello anonymous user";
+        } else {
+            return "Hello " + name;
+        }
+    }
 }
